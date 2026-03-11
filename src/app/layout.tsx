d0bc9e60be6ce1c73492b95710107be23a24a761
khaled955 <khaledmansour104@yahoo.com>
2026-03-11 15:45:08 +0300

    feat: add rose favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,5 +3,5 @@ import "./globals.css";
 import { LayoutProps } from "@/lib/types/common";
 
 export default async function RootLayout({ children }: LayoutProps) {
-  return children
+  return children;
 }
